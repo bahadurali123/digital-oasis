e87f23291427d055b39fc5622444c123b563bf5f
NadirGraphics <121675784+NadirGraphics@users.noreply.github.com>
2024-01-28 12:15:21 +0500

    fix some rout buts and add favicon.

--- app.js
+++ app.js
@@ -11,25 +11,25 @@ const Stat = require("./src/moduls/webstatus");
 const uploadfile = require("./src/middleware/multer");
 
 
-// Middleware for counting page views
-app.use(async (req, res, next) => {
-  const allowedRoutes = ['/', '/blog/blogcategory', '/blog', '/services', '/contact', '/showblog']; // Add the routes you want to track
-  // Check if the current route is in the allowed routes
-  if (allowedRoutes.includes(req.path)) {
-    const page = req.originalUrl;
-    try {
-      const stat = await Stat.findOneAndUpdate(
-        { page },
-        { $inc: { visitors: 1 } },
-        { upsert: true, new: true }
-      );
-      req.pageStats = stat;
-    } catch (error) {
-      return res.status(500).send(error.message);
-    }
-  }
-  next();
-});
+// // Middleware for counting page views
+// app.use(async (req, res, next) => {
+//   const allowedRoutes = ['/', '/blog/blogcategory', '/blog', '/services', '/contact', '/showblog']; // Add the routes you want to track
+//   // Check if the current route is in the allowed routes
+//   if (allowedRoutes.includes(req.path)) {
+//     try {
+//     const page = req.originalUrl;
+//       const stat = await Stat.findOneAndUpdate(
+//         { page },
+//         { $inc: { visitors: 1 } },
+//         { upsert: true, new: true }
+//       );
+//       req.pageStats = stat;
+//     } catch (error) {
+//       return res.status(500).send(error.message);
+//     }
+//   }
+//   next();
+// });
 // pagination Middleware
 const paginateMiddleware = async (req, res, next) => {
   try {
