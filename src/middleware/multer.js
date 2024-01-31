7bee94d424751d99482d8141d323e4bdc88f52c2
NadirGraphics <121675784+NadirGraphics@users.noreply.github.com>
2024-01-31 23:19:37 +0500

    commit

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,8 +1,8 @@
 const multer = require("multer");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        // cb(null, './public/tempfiles/');
-        cb(null, './public/');
+        cb(null, './public/tempfiles');
+        // cb(null, './public/tem');
         // cb(null, './tempfiles');
         // cb(null, './tempfiles/');
     },
