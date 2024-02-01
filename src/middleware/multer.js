437228a96460913fa141cf6e61205775e2ccb7f6
NadirGraphics <121675784+NadirGraphics@users.noreply.github.com>
2024-02-01 17:41:51 +0500

    fix multer path

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,10 +1,9 @@
 const multer = require("multer");
+const path = require("path");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/tempfiles');
-        // cb(null, './public/tem');
-        // cb(null, './tempfiles');
-        // cb(null, './tempfiles/');
+        // cb(null, './public/tempfiles');
+        cb(null, path.join(__dirname, "../../public"));
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
